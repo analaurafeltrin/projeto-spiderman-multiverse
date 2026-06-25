86e529e56ef4783e0b65a68137d1f20c635f5337
analaurafeltrina <alaura.feltrin@gmail.com>
2026-06-25 17:33:08 -0300

    refatorando classes do codigo

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,16 +1,18 @@
 function handleMouseEnter() {
-  this.classList.add('card--hovered')
+  this.classList.add('s-card--hovered')
+  document.body.id = `${this.id}-hovered`
 }
 
 function handleMouseLeave() {
-  this.classList.remove('card--hovered')
+  this.classList.remove('s-card--hovered')
+  document.body.id = ''
 }
 
 function addEventListenersToCards() {
-  const cardElements = document.getElementsByClassName('card')
+  const cardElements = document.getElementsByClassName('s-card')
 
-  for (let i = 0; i < cardElements.length; i++) {
-    const card = cardElements[i]
+  for (let index = 0; index < cardElements.length; index++) {
+    const card = cardElements[index]
     card.addEventListener('mouseenter', handleMouseEnter)
     card.addEventListener('mouseleave', handleMouseLeave)
   }
